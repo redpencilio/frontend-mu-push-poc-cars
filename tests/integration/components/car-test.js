c1fcf560e735fc13f2290d185cb3de4d780f69d6
Jan-Pieter Baert <jan-pieter@baert.jp.net>
2021-06-30 16:04:38 +0200

    Fix tests

--- tests/integration/components/car-test.js
+++ tests/integration/components/car-test.js
@@ -3,24 +3,32 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | car', function(hooks) {
+module('Integration | Component | car', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`<Car />`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(this.element.textContent.trim(), 'Brand: \nModel:');
+
+    this.set('car', {
+      brand: 'test-brand',
+      model: 'test-model',
+    });
 
     // Template block usage:
     await render(hbs`
-      <Car>
+      <Car @car={{car}}>
         template block text
       </Car>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(
+      this.element.textContent.trim(),
+      'Brand: test-brand\nModel: test-model'
+    );
   });
 });
--- tests/unit/initializers/identifier-test.js
+++ tests/unit/initializers/identifier-test.js
@@ -1,28 +1,28 @@
 import Application from '@ember/application';
 
-import { initialize } from 'cars/instance-initializers/identifier';
+import { initialize } from 'cars/initializers/identifier';
 import { module, test } from 'qunit';
 import Resolver from 'ember-resolver';
 import { run } from '@ember/runloop';
 
-module('Unit | Instance Initializer | identifier', function(hooks) {
+module('Unit | Initializer | identifier', function(hooks) {
   hooks.beforeEach(function() {
     this.TestApplication = class TestApplication extends Application {}
-    this.TestApplication.instanceInitializer({
+    this.TestApplication.initializer({
       name: 'initializer under test',
       initialize
     });
+
     this.application = this.TestApplication.create({ autoboot: false, Resolver });
-    this.instance = this.application.buildInstance();
   });
+
   hooks.afterEach(function() {
-    run(this.instance, 'destroy');
     run(this.application, 'destroy');
   });
 
   // TODO: Replace this with your real tests.
   test('it works', async function(assert) {
-    await this.instance.boot();
+    await this.application.boot();
 
     assert.ok(true);
   });
